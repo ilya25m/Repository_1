7872c4deeb4315d9b3d9976fb8de65d3eb4fa1a2
“John <john@email.com>
2026-03-13 16:13:44 +0200

    initial string info

---
+++ classwork1203.py
@@ -0,0 +1,8 @@
+dog_name = """Dog name:
+Tima
+"""
+dog_racion = """Dog racion:
+porridge
+"""
+print(dog_name + dog_racion)
+print( id(dog_name))
--- main_1.py
+++ main_1.py
@@ -1,4 +1,2 @@
-name = 'Alex'
-surname = ' Clinton'
 
-print(name)
+

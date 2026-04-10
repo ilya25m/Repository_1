db7d2d948452af8f29cd7df34f1b0ff8d3f22215
“John <john@email.com>
2026-04-10 22:29:47 +0300

    Homework done

---
+++ working_with_dictionaries.py
@@ -0,0 +1,33 @@
+car = {
+    "model": "Toyota RAV4 Hybrid",
+    "price": 1_807_000,
+    "engine_volume": 2487,
+    "total_weight": 2135,
+    "max_speed": 180,
+    "fuel_consumption": 4.8,
+
+    "interior_features": [
+        "Dual-zone climate control",
+        "Heated steering wheel",
+        "Power windows",
+        "Multimedia system",
+        "Rain sensor"
+    ],
+
+    "trunk": {
+        "volume": 580,
+        "volume_with_seats_folded": 1690
+    }
+}
+
+car["trailer_weight_with_brakes"] = 1500
+
+print("Car model:", car["model"])
+print("Price:", car["price"])
+print("First interior feature:", car["interior_features"][0])
+print("Trunk volume (seats folded):", car["trunk"]["volume_with_seats_folded"])
+
+insurance = car["price"] * 0.005
+car["insurance"] = insurance
+
+print("Insurance:", insurance)

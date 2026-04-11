c42b3bbc673355bcc40747a79637ee5b215641cb
“John <john@email.com>
2026-04-11 18:45:50 +0300

    Homework done

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import sign
 import poems
 from poems import WHEN_I_DIE_BURY_ME, IM_THIRTEEN_YEARS_OLD
 
-name = "Ілля Місюра"
-print(name)
-
 wish = "Мої побажання до поточного курсу програмування: \n Хочу більше практичних завдань \n Хочу більше прикладів коду"
 print(wish)
 

97b9c19101b1a2892891917d47acbc24f56892ed
“John <john@email.com>
2026-05-06 15:20:05 +0300

    Homework done

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-import sign
-import poems
-from poems import WHEN_I_DIE_BURY_ME, IM_THIRTEEN_YEARS_OLD
+from functions import calculate, change_text, sum_from_string
 
-wish = "Мої побажання до поточного курсу програмування: \n Хочу більше практичних завдань \n Хочу більше прикладів коду"
-print(wish)
 
-stars = "**************************************************************************"
+print('--- positional arguments ---')
+print(calculate(10, 5, "sub"))
+print(change_text("hello world", False))
+print(sum_from_string("1,2,3"))
 
-print(stars)
-print(WHEN_I_DIE_BURY_ME)
-print(stars)
-print(IM_THIRTEEN_YEARS_OLD)
-print(stars)
+
+print('\n--- named arguments ---')
+print(calculate(a=10, b=5, operation="sum"))
+print(change_text(text="python", upper=True))
+print(sum_from_string(numbers="4,5,6", separator=","))
+
+
+print('\n--- dict unpacking ---')
+calc_data = {
+    "a": 20,
+    "b": 10,
+    "operation": "sub"
+}
+
+text_data = {
+    "text": "Hello",
+    "upper": True
+}
+
+sum_data = {
+    "numbers": "7,8,9",
+    "separator": ","
+}
+
+print(calculate(**calc_data))
+print(change_text(**text_data))
+print(sum_from_string(**sum_data))
